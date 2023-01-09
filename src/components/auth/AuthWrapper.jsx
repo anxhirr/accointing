826c2fdcb4e48520c09da03ee48e02bd74cd0389
anxhirr <anxhirr@gmail.com>
2023-01-09 21:02:13 +0100

    provided login details so page visitors can access the app

--- src/components/auth/AuthWrapper.jsx
+++ src/components/auth/AuthWrapper.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
-import styles from './Auth.module.css';
+import React, { useState } from 'react'
+import styles from './Auth.module.css'
 
-import AccointingLogo from '../../images/general/logo-dark.svg';
-import Auth from './Auth';
+import AccointingLogo from '../../images/general/logo-dark.svg'
+import Auth from './Auth'
 
 const AuthWrapper = () => {
-  const [islogIn, setIsLogin] = useState(true);
+  const [islogIn, setIsLogin] = useState(true)
   return (
     <div className={styles.page}>
       <main className={styles.container}>
         <div className={styles.content}>
+          <span style={{ textAlign: 'center' }}>
+            <p>
+              For testing purpuses, feel free to use these credentials to log
+              in:
+            </p>
+            <h4>Email: anxhirr@gmail.com</h4>
+            <h4>Password: anxhi69</h4>
+          </span>
           <header className={styles.header}>
             <span>
               <img
@@ -29,7 +37,7 @@ const AuthWrapper = () => {
         </div>
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default AuthWrapper;
+export default AuthWrapper
